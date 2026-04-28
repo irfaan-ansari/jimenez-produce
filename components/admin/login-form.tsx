a6d86142aa6102010f9dbc9b43b7cecf64b08573
Irfan Ansari <irfanansari@Irfans-MacBook-Pro.local>
2026-04-28 22:08:19 +0530

    Revert login form  to previous version

--- components/admin/login-form.tsx
+++ components/admin/login-form.tsx
@@ -5,25 +5,23 @@ import z from "zod";
 import React from "react";
 import Link from "next/link";
 import { toast } from "sonner";
-import {
-  Field,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-} from "@/components/ui/field";
-import { REGEXP_ONLY_DIGITS } from "input-otp";
+import { authClient } from "@/lib/auth/client";
 import { useStore } from "@tanstack/react-form";
 import { Button } from "@/components/ui/button";
 import { useAppForm } from "@/hooks/form-context";
-import { phoneSchema } from "@/lib/form-schema/common";
+import { Field, FieldGroup } from "@/components/ui/field";
 import { AlertCircleIcon, Loader, X } from "lucide-react";
-import { InputOTP, InputOTPGroup, InputOTPSlot } from "../ui/input-otp";
 import { Alert, AlertAction, AlertDescription, AlertTitle } from "../ui/alert";
 
 const schema = z.object({
-  phoneNumber: phoneSchema,
-  code: z.string().length(6, "OTP must be 6 digits"),
-  step: z.number(),
+  username: z.union(
+    [
+      z.string().regex(/^[6-9]\d{9}$/, "Enter a valid phone number"),
+      z.email("Enter valid email"),
+    ],
+    "Enter valid email or phone number",
+  ),
+  password: z.string().min(2, "Enter password"),
   error: z.string(),
 });
 
@@ -33,38 +31,45 @@ export function LoginForm({
 }: React.ComponentProps<"div">) {
   const form = useAppForm({
     defaultValues: {
-      phoneNumber: "",
-      code: "",
-      step: 1,
-      error: "Invalid phone number",
+      username: "",
+      password: "",
+      error: "",
     },
     validators: {
-      onChange: ({ formApi, value }) => {
-        if (value.step === 1) {
-          if (value.phoneNumber.length === 10) {
-            formApi.handleSubmit();
-          } else {
-            return formApi.parseValuesWithSchema(schema);
-          }
-        }
-      },
+      onChange: schema,
     },
-    onSubmit: async ({ formApi, value }) => {
-      const { phoneNumber } = value;
+    onSubmit: async ({ value }) => {
+      const { username, password } = value;
+
       const toastId = toast.loading("Logging in...");
 
-      // send otp
+      let response;
+      if (username.includes("@")) {
+        response = await authClient.signIn.email({
+          email: username,
+          password,
+        });
+      } else {
+        response = await authClient.signIn.phoneNumber({
+          phoneNumber: username,
+          password,
+        });
+      }
 
-      formApi.setFieldValue("step", 2);
-      //   toast.success("Login successfull, redirecting...", { id: toastId });
-      //   window.location.reload();
+      if (response?.error) {
+        toast.error(response?.error?.message, { id: toastId });
+        form.setFieldValue(
+          "error",
+          response?.error?.message ?? "Login failed!",
+        );
+      } else {
+        toast.success("Login successfull, redirecting...", { id: toastId });
+        window.location.reload();
+      }
     },
   });
 
-  const { error: loginError, step } = useStore(
-    form.store,
-    (state) => state.values,
-  );
+  const loginError = useStore(form.store, (state) => state.values.error);
 
   return (
     <form
@@ -72,61 +77,43 @@ export function LoginForm({
         e.preventDefault();
         form.handleSubmit();
       }}
-      className="flex h-full flex-1 flex-col items-start justify-center gap-6 px-6 py-20 lg:px-16"
+      className="flex flex-1 flex-col items-start justify-center gap-4 px-6 py-20 lg:px-16"
     >
-      <div className="space-y-2">
-        <h2 className="text-xl font-bold">Enter phone number</h2>
-        <p className="text-sm text-muted-foreground">
-          Enter phone number to access your account.
-        </p>
-      </div>
+      <h2 className="font-heading text-3xl font-bold">Login with password</h2>
+      <p className="mb-10 text-muted-foreground">
+        Use your email address or phone number to access your account.
+      </p>
 
       <FieldGroup>
-        {step === 1 ? (
+        <form.AppField
+          name="username"
+          children={(field) => (
+            <field.TextField
+              label="Email or phone"
+              placeholder="email or phone"
+              className="*:data-[slot=input]:rounded-xl *:data-[slot=input]:bg-background"
+            />
+          )}
+        />
+
+        <div className="space-y-3">
           <form.AppField
-            name="phoneNumber"
+            name="password"
             children={(field) => (
-              <field.PhoneField
-                label="Phone Number"
-                placeholder="xxx-xxx-xxxx"
-                className="*:data-[slot=input]:rounded-lg *:data-[slot=input]:bg-background"
+              <field.PasswordField
+                label="Password"
+                placeholder="••••••"
+                className="*:data-[slot=input-group]:bg-background"
               />
             )}
           />
-        ) : (
-          <form.Field
-            name="code"
-            children={(field) => {
-              const isInvalid =
-                field.state.meta.isTouched && !field.state.meta.isValid;
-              return (
-                <Field>
-                  <FieldLabel htmlFor={field.name}>OTP</FieldLabel>
-                  <InputOTP
-                    id={field.name}
-                    name={field.name}
-                    value={field.state.value}
-                    onChange={(value) => field.handleChange(value)}
-                    onBlur={field.handleBlur}
-                    maxLength={6}
-                    pattern={REGEXP_ONLY_DIGITS}
-                  >
-                    <InputOTPGroup className="w-full flex-1 bg-background *:h-11 *:w-auto! *:flex-1! *:data-[active=true]:ring-2 *:data-[active=true]:ring-border ">
-                      <InputOTPSlot index={0} />
-                      <InputOTPSlot index={1} />
-                      <InputOTPSlot index={2} />
-
-                      <InputOTPSlot index={3} />
-                      <InputOTPSlot index={4} />
-                      <InputOTPSlot index={5} />
-                    </InputOTPGroup>
-                  </InputOTP>
-                  {isInvalid && <FieldError errors={field.state.meta.errors} />}
-                </Field>
-              );
-            }}
-          />
-        )}
+          <Link
+            href="/forgot-password"
+            className="text-sm text-muted-foreground font-medium block hover:underline hover:text-foreground"
+          >
+            Forgot Password?
+          </Link>
+        </div>
         {/* alert */}
         {loginError && (
           <Alert
@@ -134,7 +121,7 @@ export function LoginForm({
             className="rounded-xl border-destructive/5 bg-destructive/5 has-data-[slot=alert-action]:pr-8"
           >
             <AlertCircleIcon />
-            <AlertTitle>Phone number not found</AlertTitle>
+            <AlertTitle>Login Failed!</AlertTitle>
             <AlertDescription>{loginError}</AlertDescription>
             <AlertAction>
               <Button
@@ -163,24 +150,24 @@ export function LoginForm({
                 className="rounded-xl bg-sidebar-accent hover:bg-sidebar-accent/80"
                 disabled={isSubmitting || !canSubmit}
               >
-                {isSubmitting ? (
-                  <Loader className="animate-spin" />
-                ) : (
-                  "Continue"
-                )}
+                {isSubmitting ? <Loader className="animate-spin" /> : "Login"}
               </Button>
             )}
           />
         </Field>
+
+        <div className="flex flex-row items-center justify-center gap-4">
+          <div className="flex-[1_1_0] border-b "></div>
+          <span className="shrink-0">OR</span>
+          <span className="flex-[1_1_0] border-b"></span>
+        </div>
+
+        <Field className="text-center">
+          <Button type="button" size="xl" className="rounded-xl" asChild>
+            <Link href="/otp-login">Login with OTP</Link>
+          </Button>
+        </Field>
       </FieldGroup>
-      <Field className="mt-auto text-center">
-        <Link
-          href="/forgot-password"
-          className="block text-sm font-medium text-muted-foreground underline hover:text-foreground"
-        >
-          Forgot Password?
-        </Link>
-      </Field>
     </form>
   );
 }
